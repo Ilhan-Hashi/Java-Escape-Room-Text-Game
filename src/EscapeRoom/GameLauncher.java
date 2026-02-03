fb19c76c27f9ec4b9fafeef92b9388a08bef6e13
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-02-03 17:59:42 -0600

    Revert "Display a message."
    This reverts commit 8c6767bfc1e8873146b029a994e5b26fad8765a9.

--- src/EscapeRoom/GameLauncher.java
+++ src/EscapeRoom/GameLauncher.java
@@ -3,11 +3,6 @@ package EscapeRoom;
 public class GameLauncher {
 
     public static void main(String[] args) {
-        //System.out.println("Starting Game...");
-
-        // Print a message.
-        String message = "Starting Game...";
-        System.out.println(message);
-
+        System.out.println("Starting Game...");
     }
 }
