8c6767bfc1e8873146b029a994e5b26fad8765a9
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-02-03 17:43:56 -0600

    Display a message.

--- src/EscapeRoom/GameLauncher.java
+++ src/EscapeRoom/GameLauncher.java
@@ -3,6 +3,11 @@ package EscapeRoom;
 public class GameLauncher {
 
     public static void main(String[] args) {
-        System.out.println("Starting Game...");
+        //System.out.println("Starting Game...");
+
+        // Print a message.
+        String message = "Starting Game...";
+        System.out.println(message);
+
     }
 }
