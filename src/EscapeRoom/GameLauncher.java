fae1021c0ada5dfcbbf24d05ea5867548087a0a5
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-02-03 18:15:57 -0600

    Add initial exception handling using try-catch-finally blocks.

--- src/EscapeRoom/GameLauncher.java
+++ src/EscapeRoom/GameLauncher.java
@@ -1,5 +1,7 @@
 package EscapeRoom;
 
+import java.util.Scanner;
+
 public class GameLauncher {
 
     public static void main(String[] args) {
@@ -8,6 +10,25 @@ public class GameLauncher {
         // Print a message.
         String message = "Starting Game...";
         System.out.println(message);
+
+        // Set up Scanner for user input.
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Prompt for user to enter their name.
+            System.out.println("Please enter your name: ");
+            String name = scanner.nextLine();
+
+            // Initialize Player object.
+            Player player = new Player("Ilhan");
+
+            // Display a welcome message.
+            System.out.println("Welcome " + player.getName());
+        } catch (Exception ex) {
+            // Handle unexpected error.
+            System.err.println(ex.toString());
+        }
+
+
+
 
     }
 }
