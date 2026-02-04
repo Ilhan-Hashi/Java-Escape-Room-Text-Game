3e236638374db4eadc6169367b6c02824b2ea802
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-02-04 11:37:11 -0600

    Initial gane startup logging.

--- src/EscapeRoom/GameLauncher.java
+++ src/EscapeRoom/GameLauncher.java
@@ -1,6 +1,11 @@
 package EscapeRoom;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class GameLauncher {
+    // Insantiate logging object.
+    final static Logger log = LogManager.getLogger("GameLogger");
 
     public static void main(String[] args) {
         //System.out.println("Starting Game...");
@@ -8,6 +13,12 @@ public class GameLauncher {
         // Print a message.
         String message = "Starting Game...";
         System.out.println(message);
+
+        // Records when the game starts.
+        log.info("Game started");
+
+        // For debugging to confirm main() is running.
+        log.debug("Main method entered");
 
     }
 }
---
+++ src/EscapeRoom/logging.java
@@ -0,0 +1,4 @@
+package EscapeRoom;
+
+public class logging {
+}
