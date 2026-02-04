0d05e15e6f951c01802609d93e03b26935e8b637
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-02-03 18:03:17 -0600

    Revert "Added age into Player class."
    This reverts commit 7d2107627d5121c7766219268c5c7f3534e28556.

--- src/EscapeRoom/Player.java
+++ src/EscapeRoom/Player.java
@@ -10,7 +10,6 @@ package EscapeRoom;
 public class Player {
     // Private property to store the play's name.
     public String name;
-    public int age;
 
     /**
      * Constructs a Player with the specified name.
