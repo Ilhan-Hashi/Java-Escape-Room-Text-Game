98168c04426073104aad5ce47e26650e945f84b1
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-03-10 17:51:11 -0500

    Add Room class with item management and transitions.

--- src/EscapeRoom/world/Room.java
+++ src/EscapeRoom/world/Room.java
@@ -1,6 +1,8 @@
 package EscapeRoom.world;
 
+import EscapeRoom.enums.Location;
 import EscapeRoom.items.Item;
+import EscapeRoom.player.Player;
 import EscapeRoom.puzzles.Puzzle;
 
 import java.util.*;
@@ -13,4 +15,109 @@ import java.util.*;
  * @author Ilhan Hashi
  */
 public class Room {
+    //region Fields
+
+    // Private properties.
+    private String name;
+    private Location location;
+    private boolean requiresLight;
+    private Map<String, Item> items;
+    private Puzzle puzzle;
+    private RoomTransition transitionListener;
+
+    //endregion
+
+    //region Constructor
+
+    /**
+     * Creates a new room.
+     * @param name the name of the room.
+     * @param location the location of the room.
+     * @param requiresLight whether the room needs a light too see.
+     */
+    public Room(String name, Location location, boolean requiresLight) {
+        this.name = name;
+        this.location = location;
+        this.requiresLight = requiresLight;
+        this.items = new HashMap<>();
+    }
+
+    //endregion
+
+    //region Getters
+
+    public String getName() {return name;}
+    public Location getLocation() {return location;}
+    public boolean requiresLight() {return requiresLight;}
+    public Puzzle getPuzzle() {return puzzle;}
+
+    //endregion
+
+    //region Setters
+
+    public void setPuzzle(Puzzle puzzle) {this.puzzle = puzzle;}
+
+    //endregion
+
+    //region Item Movements
+
+    /**
+     * Adds an item to the room.
+     * @param item the item that is being added to the room.
+     */
+    public void addItem(Item item) {
+        items.put(item.getName().toLowerCase(), item);
+    }
+
+    public Map<String, Item> getItems() {
+        return items;
+    }
+
+    //endregion
+
+    //region Actions
+
+    /**
+     * Handles examining an item in the room.
+     */
+    public void onExamine(String itemName, Player player, boolean flashlightOn) {
+        System.out.println("You don't see anything like that here.");
+    }
+
+    /**
+     * Handles taking an item.
+     */
+    public void onTake(String itemName, Player player, boolean flashlightOn) {
+        System.out.println("You don't see anything like that here.");
+    }
+
+    /**
+     * Handles using an item.
+     */
+    public void onUse(String itemName, Player player, boolean flashlightOn) {
+        System.out.println("You don't use that here.");
+    }
+
+    //endregion
+
+    //region Room Transition
+
+    /**
+     * Sets the room transition listener.
+     */
+    public void setTransitionListener(RoomTransition transitionListener) {
+        this.transitionListener = transitionListener;
+    }
+
+    /**
+     * Transitions the player to the specified room.
+     * @param destination the location being transition to.
+     */
+    public void transitionTo(Location destination) {
+        if (transitionListener != null) {
+            transitionListener.transition(destination);
+        }
+    }
+
+    //endregion
 }
