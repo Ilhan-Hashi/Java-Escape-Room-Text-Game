8027feb489eaf78bd1128c9ae2b71f726ecbd179
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-03-10 18:31:27 -0500

    Add Tool item class.

--- src/EscapeRoom/items/Tool.java
+++ src/EscapeRoom/items/Tool.java
@@ -9,4 +9,51 @@ import EscapeRoom.player.Player;
  */
 public class Tool extends Item implements Usable {
 
+    //region Fields
+
+    private boolean active;
+
+    //endregion
+
+    //region Constructor
+
+    /**
+     * Creates a tool.
+     * @param name tbe name of the tool.
+     * @param description tool description.
+     */
+    public Tool(String name, String description) {
+        super(name, description);
+        this.active = false;
+    }
+
+    //endregion
+
+    //region Getters
+
+    public boolean isActive() {return active;}
+
+    //endregion
+
+    //region void Use
+
+    /**
+     * Uses the tool.
+     * @param player the player using the item.
+     */
+    @Override
+    public void use(Player player) {
+        boolean turningOn = !active;
+        active = turningOn;
+
+        if (turningOn) {
+            System.out.println("You turn on the " + getName() + ".");
+            System.out.println();
+        } else {
+            System.out.println("You turn off the " + getName() + ".");
+            System.out.println();
+        }
+    }
+
+    //endregion
 }
