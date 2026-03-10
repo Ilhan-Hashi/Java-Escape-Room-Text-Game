09fc59021d4a3f33e331cd01d99a3d3472ace6e1
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-03-10 18:14:43 -0500

    Add Key item class.

--- src/EscapeRoom/items/Key.java
+++ src/EscapeRoom/items/Key.java
@@ -7,6 +7,32 @@ import EscapeRoom.player.Player;
  *
  * @author Ilhan Hashi
  */
-public class Key extends Item implements Usable {
+public class Key extends Item {
+    //region Fields
 
+    private String unlocks;
+
+    //endregion
+
+    //region Constructor
+
+    /**
+     * Creates a key.
+     *
+     * @param name the name of a key.
+     * @param description the key description.
+     * @param unlocks what the key unlocks.
+     */
+    public Key(String name, String description, String unlocks) {
+        super(name, description);
+        this.unlocks = unlocks;
+    }
+
+    //endregion
+
+    //region Getters
+
+    public String getUnlocks() {return unlocks;}
+
+    //endregion
 }
