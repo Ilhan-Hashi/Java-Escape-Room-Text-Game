8bcce242d67e13f2776e8b0cb67ef119ce56d0de
Ilhan Hashi <ilhan.hashi@bit.nwtc.edu>
2026-03-10 23:54:44 -0500

    Add CommandHandler class for parsing player command.

--- src/EscapeRoom/game/CommandHandler.java
+++ src/EscapeRoom/game/CommandHandler.java
@@ -1,5 +1,7 @@
 package EscapeRoom.game;
 
+import EscapeRoom.enums.Direction;
+
 /**
  * The CommandHandler class is responsible
  * for parsing user input and calling the
@@ -8,5 +10,44 @@ package EscapeRoom.game;
  * @author Ilhan Hashi
  */
 public class CommandHandler {
+    //region Fields
 
+    private GameManager gameManager;
+
+    //endregion
+
+    //region Constructor
+
+    /**
+     * Creates a command handler for the game.
+     * @param gameManager the game manager used to process commands.
+     */
+    public CommandHandler(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+    //endregion
+
+    //region Direction Parser
+
+    /**
+     * Converts text input into a Direction
+     * @param text the direction entered by the player.
+     * @return the matching Direction or null if invalid.
+     */
+    private Direction parseDirection(String text) {
+
+        switch (text.toLowerCase()) {
+
+            case "north": case "n": return Direction.NORTH;
+            case "south": case "s": return Direction.SOUTH;
+            case "east":  case "e": return Direction.EAST;
+            case "west":  case "w": return Direction.WEST;
+            case "center": case "c": return Direction.CENTER;
+
+            default: return null;
+        }
+    }
+
+    //endregion
 }
